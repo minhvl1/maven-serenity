9f5d0efe2c857ea958904d764d6ccb234c79eae7
minh vu <minhvu890yahoo@gmail.com>
2022-12-04 22:12:01 +0700

    add radio button feature

--- src/test/java/Steps/DemoQaSteps.java
+++ src/test/java/Steps/DemoQaSteps.java
@@ -6,14 +6,19 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
+import org.junit.Assert;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+
 @RunWith(SerenityRunner.class)
 public class DemoQaSteps {
+
+
     @Managed(driver = "edge",options = "--headless")
     WebDriver driver;
+
 //    public WebDriver driver;
 //    public DemoQaSteps(){
 //        this.driver = BaseTest.openAndQuitBrowser();
@@ -31,5 +36,21 @@ public class DemoQaSteps {
     @Then("See result after input textbox")
     public void seeResultAfterInputTextbox() {
         System.out.println(driver.getCurrentUrl());
+    }
+
+    @Given("I go to demo qa radio button url")
+    public void iGoToDemoQaRadioButtonUrl() {
+        driver.get("https://demoqa.com/radio-button");
+    }
+
+    @When("Click {string} button")
+    public void clickRadioButton(String button) {
+        driver.findElement(By.xpath("//label[@for="+"'"+button.toLowerCase()+"Radio']")).click();
+    }
+
+    @Then("See result contain {string}")
+    public void seeResultContainRadio(String button) {
+        System.out.println("result:"+driver.findElement(By.xpath("//p[@class='mt-3']")).getText());
+        Assert.assertTrue(driver.findElement(By.xpath("//p[@class='mt-3']")).getText().contains(button));
     }
 }
--- src/test/resources/features/demoQA.feature
+++ src/test/resources/features/demoQA.feature
@@ -6,3 +6,12 @@ Feature: screen play demo
     When input "minh" fullname textbox
     Then See result after input textbox
 
+  @radiobutton
+  Scenario Outline: Radio button
+    Given I go to demo qa radio button url
+    When  Click <radio> button
+    Then  See result contain <radio>
+    Examples:
+      | radio |
+    |   "Impressive"    |
+    |     "Yes"|
