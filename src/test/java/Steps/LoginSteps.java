396086b914542548c82f68df7ea553111407ed36
minhvl1 <minhvu890yahoo@gmail.com>
2023-01-31 16:17:24 +0700

    add log4j

--- src/test/java/Steps/LoginSteps.java
+++ src/test/java/Steps/LoginSteps.java
@@ -6,6 +6,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
+import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
@@ -16,6 +17,7 @@ import org.openqa.selenium.WebDriver;
 public class LoginSteps {
 //    @Managed(driver = "edge",options = "--headless")
 //    WebDriver driver;
+private static final Logger logger = Logger.getLogger(LoginSteps.class);
     @Managed
     WebDriver driver;
 //    public LoginSteps(){
@@ -24,27 +26,32 @@ public class LoginSteps {
 
     @Given("I go to google")
     public void iGoToGoogle() {
+//        logger.info("===Open browser===");
         driver.get("https://www.google.com/");
     }
 
     @When("search {string}")
     public void search(String arg0) {
+//        logger.info("===Search keyword===");
         driver.findElement(By.xpath("//input[@name='q']")).sendKeys(arg0);
     }
 
     @When("click search button")
     public void clickSearchButton() {
+//        logger.info("===Click search button===");
         driver.findElement(By.xpath("//input[@name='q']")).sendKeys(Keys.RETURN);
     }
 
     @Then("See result")
     public void seeResult() {
+//        logger.info("===See result page===");
         String url =driver.getCurrentUrl();
         System.out.println(url);
     }
 
     @And("URL contains {string}")
     public void urlContains(String arg0) {
+//        logger.info("===Verify result===");
         String url =driver.getCurrentUrl();
         Assert.assertTrue(url.contains(arg0));
     }
